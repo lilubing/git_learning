15eea5c9ee816b0b32c15d1e3adae4cef0ee1896
lilubing <8366419@qq.com>
2021-02-25 07:16:34 +0800

    Create a complete web page Add index + images Add styles.css Add js Add a click

---
+++ js/cc.js
@@ -0,0 +1,8 @@
+window.onload=function(){ 
+        var btn = document.getElementById("swf");
+
+        // 第一种 通过点击事件
+        btn.onclick = function(){
+            alert("This is click");
+        }
+    }
